536cb569ef9370a094ffe69d6e9c5744f96cfd4b
Alicia <alicia.feliciano@zup.com.br>
2024-12-30 12:27:04 -0300

    Including inclusion in the pom.xml of the vallidation library, in addition to the necessary validations in the response and request address DTOS

--- address-api/src/main/java/br/com/zup/address/controllers/dtos/AddressRequestDTO.java
+++ address-api/src/main/java/br/com/zup/address/controllers/dtos/AddressRequestDTO.java
@@ -1,52 +1,36 @@
 package br.com.zup.address.controllers.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.NotFound;
+
+@Getter
+@Setter
 public class AddressRequestDTO {
+
+    @NotBlank(message = "The field cannot be empty.")
+    @Size(max = 100, message = "The 'street' field must have a maximum of 100 characters.")
     private String street;
+
+    @NotBlank(message = "The field cannot be empty.")
+    @Size(max = 50, message = "The 'city' field must have a maximum of 50 characters.")
     private String city;
+
+    @NotBlank(message = "The field cannot be empty.")
+    @Size(max = 10, message = "The 'zipCode' field must have a maximum of 10 characters.")
     private String zipCode;
+
+    @NotBlank(message = "The field cannot be empty.")
+    @Size(max = 2, message = "The 'state' field must have a maximum of 2 characters.")
     private String state;
+
+    @NotBlank(message = "The field cannot be empty.")
+    @Size (max = 36, message = "The 'consumerId' field must have a maximum of 36 characters.")
     private String consumerId;
 
     public AddressRequestDTO() {
     }
 
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getConsumerId() {
-        return consumerId;
-    }
-
-    public void setConsumerId(String consumerId) {
-        this.consumerId = consumerId;
-    }
 }
