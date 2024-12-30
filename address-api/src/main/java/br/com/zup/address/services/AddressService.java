9fb365d9d04a4042198184242b3d55bcb09cd5cc
Alicia <alicia.feliciano@zup.com.br>
2024-12-30 14:53:29 -0300

    Updating the CRAS service address but adding code monitoring treatment with logger

--- address-api/src/main/java/br/com/zup/address/services/AddressService.java
+++ address-api/src/main/java/br/com/zup/address/services/AddressService.java
@@ -4,6 +4,8 @@ import br.com.zup.address.models.Address;
 import br.com.zup.address.repositories.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,40 +13,49 @@ import java.util.Optional;
 @Service
 public class AddressService {
 
+    private static final Logger logger = LoggerFactory.getLogger(AddressService.class);
+
     @Autowired
     private AddressRepository addressRepository;
 
-    // Create
+
     public Address createAddress(Address address) {
+        logger.info("Creating a new address: {}", address);
         return addressRepository.save(address);
     }
 
-    // Read (Get All)
     public List<Address> getAllAddresses() {
+        logger.info("Fetching all addresses");
         return addressRepository.findAll();
     }
 
-    // Read (Get by ID)
     public Optional<Address> getAddressById(String id) {
+        logger.info("Fetching address with id: {}", id);
         return addressRepository.findById(id);
     }
 
-    // Update
     public Address updateAddress(String id, Address updatedAddress) {
+        logger.info("Updating address with id: {}", id);
         return addressRepository.findById(id).map(address -> {
             address.setStreet(updatedAddress.getStreet());
             address.setCity(updatedAddress.getCity());
             address.setState(updatedAddress.getState());
             address.setZipCode(updatedAddress.getZipCode());
+            logger.info("Address updated: {}", address);
             return addressRepository.save(address);
-        }).orElseThrow(() -> new RuntimeException("Address not found with id " + id));
+        }).orElseThrow(() -> {
+            logger.error("Address not found with id: {}", id);
+            return new RuntimeException("Address not found with id " + id);
+        });
     }
 
-    // Delete
     public void deleteAddress(String id) {
+        logger.info("Deleting address with id: {}", id);
         if (addressRepository.existsById(id)) {
             addressRepository.deleteById(id);
+            logger.info("Address deleted with id: {}", id);
         } else {
+            logger.error("Address not found with id: {}", id);
             throw new RuntimeException("Address not found with id " + id);
         }
     }
