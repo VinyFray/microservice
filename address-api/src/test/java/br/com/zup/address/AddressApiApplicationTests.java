63cc6808cc5df5f517fbac5b652c32c7dfa00e86
Alicia <alicia.feliciano@zup.com.br>
2024-12-30 15:52:43 -0300

    Uploading tests from the address (application test)

--- address-api/src/test/java/br/com/zup/address/AddressApiApplicationTests.java
+++ address-api/src/test/java/br/com/zup/address/AddressApiApplicationTests.java
@@ -1,13 +1,33 @@
 package br.com.zup.address;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AddressApiApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void shouldReturnDefaultMessage() {
+		String url = "http://localhost:" + port + "/addresses";
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+		assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
+		assertThat(response.getBody()).contains("[]");
+	}
 }
