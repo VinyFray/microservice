4ab436bd500235e58eba0a48c5d904d25a2eeafc
Alicia <alicia.feliciano@zup.com.br>
2024-12-31 12:50:57 -0300

    adding validations

--- gateway/src/main/java/br/com/zup/gateway/controllers/dtos/ConsumerAddressResponseDTO.java
+++ gateway/src/main/java/br/com/zup/gateway/controllers/dtos/ConsumerAddressResponseDTO.java
@@ -2,13 +2,33 @@ package br.com.zup.gateway.controllers.dtos;
 
 import br.com.zup.gateway.infra.clients.address.dtos.AddressResponseDTO;
 import br.com.zup.gateway.infra.clients.consumer.dtos.ConsumerResponseDTO;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class ConsumerAddressResponseDTO {
+
+    @NotBlank
     private String id;
+
+    @NotBlank(message = "The name cannot be blank" )
+    @Size(min = 3, max = 20)
     private String name;
+
+    @NotBlank(message = "Age cannot be blank")
+    @Size(max = 130)
     private String age;
+
+    @NotBlank(message = "Email cannot be blank" )
+    @Email(message = "Out of the norm")
     private String email;
+
+    @NotBlank
     private AddressDTO address;
+
+    public ConsumerAddressResponseDTO() {
+
+    }
 
     public ConsumerAddressResponseDTO(ConsumerResponseDTO consumerResponseDTO, AddressResponseDTO addressResponseDTO) {
         this.id = consumerResponseDTO.getId();
@@ -16,45 +36,5 @@ public class ConsumerAddressResponseDTO {
         this.age = consumerResponseDTO.getAge();
         this.email = consumerResponseDTO.getEmail();
         this.address = new AddressDTO(addressResponseDTO);
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public AddressDTO getAddress() {
-        return address;
-    }
-
-    public void setAddress(AddressDTO address) {
-        this.address = address;
     }
 }
